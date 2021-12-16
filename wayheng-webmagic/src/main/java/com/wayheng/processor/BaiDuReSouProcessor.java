dc15e6a19f72fffc3d0d321a1a1f1812c2d84481
wayheng <1257675427@qq.com>
2021-12-16 15:53:04 +0800

    创建了webmagic项目

---
+++ wayheng-webmagic/src/main/java/com/wayheng/processor/BaiDuReSouProcessor.java
@@ -0,0 +1,24 @@
+package com.wayheng.processor;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * 处理器-百度热搜
+ * @author wayheng
+ *
+ */
+public class BaiDuReSouProcessor implements PageProcessor {
+
+	@Override
+	public void process(Page page) {
+		page.putField("titles", page.getHtml().xpath("//div[@class='c-single-text-ellipsis']/allText()").all());
+	}
+
+	@Override
+	public Site getSite() {
+		return Site.me().setRetryTimes(3).setSleepTime(1000).setUseGzip(false);
+	}
+
+}
---
+++ wayheng-webmagic/src/main/java/com/wayheng/spider/BaiduReSouSpider.java
@@ -0,0 +1,25 @@
+package com.wayheng.spider;
+
+import java.util.List;
+
+import com.wayheng.processor.BaiDuReSouProcessor;
+
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Spider;
+
+public class BaiduReSouSpider {
+
+	public static void main(String[] args) {
+		//single download
+        Spider spider = Spider.create(new BaiDuReSouProcessor()).thread(1);
+        String urlTemplate = "https://top.baidu.com/board?tab=realtime";
+        ResultItems resultItems = spider.<ResultItems>get(urlTemplate);
+        List<String> titles = resultItems.get("titles");
+        int size = titles.size();
+        for (int i = 0; i < size; i++) {
+			System.out.println(titles.get(i));
+		}
+        spider.close();
+	}
+	
+}
