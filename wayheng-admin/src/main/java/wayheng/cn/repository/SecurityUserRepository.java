a611bd47a1ada235569adfb170111c87623821f4
wayheng <wayheng@qq.com>
2025-03-09 17:59:57 +0800

    提交代码

--- wayheng-admin/src/main/java/wayheng/cn/repository/SecurityUserRepository.java
+++
@@ -1,7 +0,0 @@
-package wayheng.cn.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wayheng.cn.entity.SecurityUserEntity;
-
-public interface SecurityUserRepository extends JpaRepository<SecurityUserEntity, String> {
-}
